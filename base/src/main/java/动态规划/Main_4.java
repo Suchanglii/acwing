45eb2ad0928aed38bc38d97656cd0ef51b159404
Suchanglii <junfuzhao3@gmail.com>
2023-10-17 10:24:01 +0800

    多重背包,使用二进制优化理解

--- base/src/main/java/动态规划/Main_4.java
+++ base/src/main/java/动态规划/Main_4.java
@@ -13,24 +13,33 @@ public class Main_4 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-//        for (int i = 1; i <=n; i++) {
-//        s[i] = sc.nextInt();
-//            v[i] = sc.nextInt();
-//            w[i] = sc.nextInt();
-//        }
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             int v = sc.nextInt();
             int w = sc.nextInt();
             int s = sc.nextInt();
-            for (int j = m; j >= 0; j--) {
-                for (int k = 1; k <= s && k * v <= j; k++) {
+            for (int j = m; j >= 0; j--) {//这里可以看成是多个物品,选育不选,就是01背包了,而不是完全背包
+                for (int k = 0; k <= s && k * v <= j; k++) {
+                    //f[i][j] = f[i - 1][j];不需要
+                    //if (k == 0) f[i][j] = max(f[i - 1][j], f[i - 1][j - k * v[i]] + k * w[i]);
+                    //------------------------------
+                    //f[i][j] = Math.max(f[i][j], f[i - 1][j - k * v] + k * w);//朴素做法
+
                     f[j] = Math.max(f[j], f[j - k * v] + k * w);
                 }
+                //如果按照01背包的写法那么f[i][j]应该从f[i-1][j]和f[i-1][j-k*v[i]]+k*w[i]转移过来,但是这里max第一项写的是f[i][j].
+                //f[i][j] = max(f[i-1][j], f[i - 1][j - k * v[i]] + k * w[i]);//惯常思路
+                //写f[i-1][j]错误的原因是,取不同k下标的值只要大于f[i-1][j]就会被更新,而事实上要找的是取不同k下标的最大值,所以max第一项要写成f[i][j]
+                //举个例子,有一个守擂台a和一个挑战者集合B,如果B中有人打败a就应该替换a,让别人挑战自己;而不是挑战者只能和a打,打败a就能守擂.
+                //那么你会问f[i][j]一开始不是0吗,不取f[i-1][j],不会漏掉情况吗?事实上不会,当k=0时,f[i][j] = max(f[i][j],f[i-1][j]),这一步就会把f[i-1][j]的值转移过来
+                //以上简介来自:https://zhuanlan.zhihu.com/p/368901684
             }
         }
-
         System.out.println(f[m]);
-
+        //------------------------------
+        // int res = 0;
+        // for (int i = 0; i <= m; i++) {
+        //     res = Math.max(res, f[n][i]);
+        // }
+        // System.out.println(res);
     }
 }
--- base/src/main/java/动态规划/Main_5.java
+++ base/src/main/java/动态规划/Main_5.java
@@ -1,4 +1,47 @@
 package 动态规划;
 
+//使用二进制优化
+
+import java.util.Scanner;
+import java.util.Vector;
+
 public class Main_5 {
+    static int N = 2010;
+    static int f[] = new int[N];
+
+    static Vector<Good> goods = new Vector();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            int v, w, s;
+            v = sc.nextInt();
+            w = sc.nextInt();
+            s = sc.nextInt();
+            for (int k = 1; k <= s; k *= 2) {
+                s -= k;
+                goods.add(new Good(k * v, k * w));
+            }
+            if (s > 0) goods.add(new Good(s * v, s * w));
+        }
+
+        for (Good good : goods) {
+            for (int j = m; good.v <= j; j--) {
+                f[j] = Math.max(f[j], f[j - good.v] + good.w);
+            }
+        }
+
+        System.out.println(f[m]);
+    }
+}
+
+class Good {
+    int v, w;
+
+    public Good(int v, int w) {
+        this.v = v;
+        this.w = w;
+    }
 }
