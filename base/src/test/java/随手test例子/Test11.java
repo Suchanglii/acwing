e0811a0bb06e2d1a82b43797fb915861d95d6ed0
Suchanglii <junfuzhao3@gmail.com>
2023-10-26 15:53:09 +0800

    提交一些随手写的例子

--- base/src/test/java/随手test例子/Test11.java
+++ base/src/test/java/随手test例子/Test11.java
@@ -11,24 +11,25 @@ public class Test11 extends Date {
 //        创建了一个StringBuilder对象,一次调用append(),这里还执行了StringBuilder的toString()方法,不然变不成String对象
 //        System.out.println(s.hashCode());
 //        System.out.println(s.getClass().getName());
-
-        String str = new String();
-        for(int i=0;i<100;i++)
-        {
-            str = str + i;
-            //总共会创建200个对象....
-        }
+// ---------------------------------------------------------
+//        String str = new String();
+//        for(int i=0;i<100;i++)
+//        {
+//            str = str + i;
+//            //总共会创建100个对象....
+//        }
 //----------------------
 
-//        new 随手test例子.Test11().test();
+        Test11 test11 = new Test11();
+        
     }
-//    public void test() {
-//        System.out.println(this.getClass().hashCode());
-//        System.out.println(super.getClass().hashCode());
-//        System.out.println(this.getClass());
-//        System.out.println(super.getClass());
-//
-//        System.out.println(super.getClass().getName());
-//    }
+    public void test() {
+        System.out.println(this.getClass().hashCode());
+        System.out.println(super.getClass().hashCode());
+        System.out.println(this.getClass());
+        System.out.println(super.getClass());
+
+        System.out.println(super.getClass().getName());
+    }
 
 }
---
+++ base/src/test/java/随手test例子/TestThreadPool.java
@@ -0,0 +1,31 @@
+package 随手test例子;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestThreadPool {
+    public static void main(String[] args) {
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 10; i++) {
+            pool.execute(new Runnable() {
+                @Override
+                public void run() {
+
+                }
+            });
+        }
+        Executors.newCachedThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
+        Executors.newScheduledThreadPool(3).execute(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+   }
+}
